71a3042303a45f55876f5d3479f84803ed3e71b9
lortizc3 <lortizc3@eafit.edu.co>
2024-04-01 17:51:25 -0500

    OfferClass

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/offers/list', 'App\Http\Controllers\OfferController@list')->name('offer.list');
+Route::get('/offers/create', 'App\Http\Controllers\OfferController@create')->name('offer.create');
+Route::post('/offers/save', 'App\Http\Controllers\OfferController@save')->name('offer.save');
+Route::get('/offers/delete-{id}', 'App\Http\Controllers\OfferController@delete')->name('offer.delete');
+Route::get('/offers/{id}', 'App\Http\Controllers\OfferController@show')->name('offer.show');
+
+
+
