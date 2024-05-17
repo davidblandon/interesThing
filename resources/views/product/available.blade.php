2e17b02f24091d82bdb507df31f60a34b0c79007
davidblandon <dablandonr@eafit.edu.co>
2024-05-16 22:34:51 -0500

    Image upload and search function implemented

--- resources/views/product/available.blade.php
+++ resources/views/product/available.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 @section('title', $viewData["title"])
 @section('content')
-<div style="position: absolute; top: 2; right: 7px;;">
-  <div class="input-group">
-    <div class="form-outline" data-mdb-input-init>
-      <input type="search" id="form1" class="form-control" placeholder="Search" />
+<div style="position: absolute; top: 2; right: 7px;">
+  <form action="{{ route('product.available') }}" method="GET">
+    <div class="input-group">
+      <div class="form-outline" data-mdb-input-init>
+        <input type="search" name="search" id="form1" class="form-control" placeholder="Search" value="{{ request()->input('search') }}" />
+      </div>
+      <button type="submit" style="background-color: #71A06C" class="btn btn-primary" data-mdb-ripple-init>
+        <i class="fas fa-search"></i>
+      </button>
     </div>
-    <button type="button" style="background-color: #71A06C" class="btn btn-primary" data-mdb-ripple-init>
-      <i class="fas fa-search"></i>
-    </button>
-  </div>
-</div>
+  </form>
 </div>
 <div class="container">
 <div class="row">
         @foreach($viewData['products'] as $product)
             <div class="col">
                 <div class="product">
-                    <img src="{{ url('/images/ropaTopicos.jpeg') }}" alt="{{ $product->name }}">
+                    <img src="{{ asset('storage/' . $product->getPhoto()) }}" class="card-img-top" alt="...">
                     <h2>{{ $product->name }}</h2>
                     <!-- Suponiendo que 'description' sea un atributo en tu modelo -->
                     <p>{{ $product->description }}</p>
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -3,11 +3,10 @@
 @section('subtitle', $viewData["subtitle"])
 @section('content')
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="{{ asset('storage/' . $viewData["product"]->getPhoto()) }}" class="card-img-top" alt="...">
   <div class="card-body" style="background-color: #F9DE96">
     <h5 class="card-title">{{ $viewData["product"]["name"] }}</h5>
     <p class="card-text">{{ $viewData["product"]["description"] }}  </p>
-    <p class="card-text">{{ $viewData["product"]["photo"] }}</p>
     <p class="card-text">{{ $viewData["product"]["category"] }}</p>
     <p class="card-text">{{ $viewData["product"]["price"] }}</p>
     <p class="card-text">{{ $viewData["product"]->getSeller()->getName() }}</p>
